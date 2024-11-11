7043b95ddbe794d02d4324b6871214b1307fff20
bryan-generatives <alfredo@generatives.co>
2024-11-11 12:23:21 +0800

    setup

--- setup.py
+++ setup.py
@@ -9,5 +9,13 @@ setup(
         "streamlit>=1.32.0",
         "beautifulsoup4>=4.12.3",
         "requests>=2.31.0",
+        "langchain==0.3.7",
+        "langchain_community==0.3.5",
+        "nltk==3.8.1",
+        "openai==1.54.3",
+        "python-dotenv==1.0.1",
+        "setuptools==59.6.0",
+        "streamlit==1.40.0",
+        "tiktoken",
     ],
 )
